a642b8599d84fdb69ab416b78e248b7657f19b2b
user <user@cseui-MacBookPro.local>
2025-10-02 02:26:04 +0900

    chore: 초기 화면 연결

---
+++ caplog/caplog/caplogApp.swift
@@ -0,0 +1,17 @@
+//
+//  caplogApp.swift
+//  caplog
+//
+//  Created by user on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CaplogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Start()
+        }
+    }
+}
