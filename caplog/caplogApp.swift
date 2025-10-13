b394f3bfd3930007cf5f3ab195eb7b5fedc05a50
minha222 <minha2469@ewhain.net>
2025-10-13 17:23:41 +0900

    fix: flatten nested caplog structure (kept AppDelegate version)

--- caplog/caplogApp.swift
+++ caplog/caplogApp.swift
@@ -1,10 +1,39 @@
 import SwiftUI
+import UIKit
+import KakaoSDKCommon
+import KakaoSDKAuth
+import GoogleSignIn
 
 @main
 struct CaplogApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    init() {
+        KakaoSDK.initSDK(appKey: "81b506b612e5cc41201bc15a145764cb")
+    }
+
     var body: some Scene {
         WindowGroup {
-            HomeView()   // 앱 시작 진입점은 임시로 Home
+            StartView()
+                .onOpenURL { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    } else {
+                        _ = GIDSignIn.sharedInstance.handle(url)
+                    }
+                }
         }
+    }
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    @available(iOS, deprecated: 26.0)
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            return AuthController.handleOpenUrl(url: url)
+        }
+        return GIDSignIn.sharedInstance.handle(url)
     }
 }
